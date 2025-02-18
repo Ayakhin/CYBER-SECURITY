feda3611120db3d8d1aace8fbd922db127ac690c
Ayakhin <rayanhilliman@gmail.com>
2025-02-18 12:33:54 +0100

    last commit

---
+++ cgi-bin/hello.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+print(
+    """\
+Content-Type: text/html
+
+<!DOCTYPE html>
+<html lang="en">
+<head><title>CGI Script</title></head>
+<body>
+<h1>Hello, World!</h1>
+<p>This is a Python CGI script running on a local server.</p>
+</body>
+</html>"""
+)
